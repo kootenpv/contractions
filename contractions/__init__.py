c3dbf6e4cec3c9dba8f0e21215ed8c50b2abebe0
Pascal Vankooten <pascal.vankooten@leaseplan.com>
2019-12-03 23:36:57 +0100

    fixes #11

--- contractions/__init__.py
+++ contractions/__init__.py
@@ -1,3 +1,4 @@
+from itertools import product
 from textsearch import TextSearch
 
 
@@ -164,9 +165,36 @@ leftovers_dict.update({k.replace("'", "’"): v for k, v in leftovers_dict.items
 
 safety_keys = set(["he's", "he'll", "we'll", "we'd", "it's", "i'd", "we'd", "we're"])
 
-unsafe_dict = {
-    k.replace("'", ""): v for k, v in contractions_dict.items() if k.lower() not in safety_keys
-}
+
+def get_combinations(tokens, joiners):
+    combs = []
+    combs.append(tokens)
+    results = []
+    for option in combs:
+        option = [[x] for x in option]
+        option = intersperse(option, joiners)
+        for c in product(*option):
+            results.append("".join(c))
+    return results
+
+
+def intersperse(lst, item):
+    result = [item] * (len(lst) * 2 - 1)
+    result[0::2] = lst
+    return result
+
+
+# have to create all the possibilities of ' and nothing
+unsafe_dict = {}
+for k, v in contractions_dict.items():  # contractions_dict.items():
+    if k.lower() in safety_keys:
+        continue
+    if "'" not in k:
+        continue
+    tokens = k.split("'")
+    for comb in get_combinations(tokens, ["", "'"]):
+        unsafe_dict[comb] = v
+
 
 slang_dict = {
     "ima": "I am going to",

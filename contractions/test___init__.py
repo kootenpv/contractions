d3db794d414f7c094ac6be464c6ecdbdabfcbebb
pascal <pascal@vks.ai>
2020-06-10 23:29:05 +0200

    added test for #23

--- contractions/test___init__.py
+++ contractions/test___init__.py
@@ -1,9 +1,17 @@
 import pytest
 import contractions
 
+
 def test_fix():
-    assert contractions.fix("you're happy now")=="you are happy now"
+    assert contractions.fix("you're happy now") == "you are happy now"
+
 
 def test_add():
     contractions.add('mychange', 'my change')
-    assert contractions.fix('mychange')=='my change'
+    assert contractions.fix('mychange') == 'my change'
+
+
+def test_ill():
+    txt = 'He is to step down at the end of the week due to ill health'
+    assert contractions.fix(txt) == txt
+    assert contractions.fix("I'll") == "I will"

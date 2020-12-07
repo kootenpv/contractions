80afae7af105cf25abd5c6fee186fc50ba9460ed
pascal <pascal@vks.ai>
2020-12-07 11:45:45 +0100

    initpy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 MAJOR_VERSION = "0"
 MINOR_VERSION = "0"
-MICRO_VERSION = "40"
+MICRO_VERSION = "41"
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(
